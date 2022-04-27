0804d7b3dbe6751d9043fa46a72f163801d324ef
ryu <munam4@naver.com>
2022-04-27 22:15:15 +0900

    feat: develop a Room Model, create room serializers

---
+++ server/rooms/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.conf import settings
+
+
+# Create your models here.
+class Room(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='rooms')
+    isStart = models.BooleanField(default=False)
+    roomtitle = models.CharField(max_length=50)
+    roomdetail = models.CharField(max_length=300)
+    nowpersons = models.TextField()
+    persons = models.IntegerField()
+    endtime = models.DateField()
+    linkURL = models.TextField()
+    roomTimeStr = models.DateField()
---
+++ server/rooms/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Room
+
+
+class RoomListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'roomtitle', 'nowpersons', 'persons', 'endtime', 'roomTimeStr')
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
