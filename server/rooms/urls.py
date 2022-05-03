778eea197b8cd781d615f7163d6b50d8f1bb6cd4
ryu <munam4@naver.com>
2022-05-03 23:39:21 +0900

    feat: add a room make logic and enter room logic

--- server/rooms/urls.py
+++ server/rooms/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import RoomCreateView
+from .views import RoomCreateView, ReadyView, EnterRoomView
 urlpatterns = [
     path('createroom', RoomCreateView.as_view()),
-
+    path('ready', ReadyView.as_view()),
+    path('enterroom', EnterRoomView.as_view()),
 ]
--- server/rooms/views.py
+++ server/rooms/views.py
@@ -45,22 +45,100 @@ class RoomCreateView(APIView):
             return response
         
         serializer = RoomSerializer(data=request.data)
-        print("통과?")
+        # print("통과?")
         if serializer.is_valid(raise_exception=True):
             serializer.save(user=user)
             room = Room.objects.filter(user_id = pk).first()
             room.persons = 1
             room.save()
             user.enteredRoom = room.pk
+            user.isMakingRoom = True
             user.save()
             response = Response()
             response.headers = {
                 'access_token':new_access_token,
             }
             response.data = {
-                'message' : 'success',
-                'image' : user.image
+                'message' : 'success'
+                # room pk? 
             }
             response.status_code = status.HTTP_200_OK
             return response
             
+class ReadyView(APIView):
+    def post(self, request):
+        access_token = request.headers.get("access-token")
+        refresh_token = request.headers.get("refresh-token")
+        pk = request.data['pk']
+        user = get_object_or_404(get_user_model(), pk=pk)
+        new_access_token, checkError = CheckToken(access_token,refresh_token,int(pk))
+
+        if checkError:
+            user.refreshToken = ''
+            user.save()
+            response = Response()
+            response.data = { 
+                'message' : 'failed',
+                'detail': 'Token Error',
+            }
+            response.status_code = status.HTTP_401_UNAUTHORIZED
+            return response
+        
+        user.isReady = True
+        user.save()
+
+        roompk = request.data['roompk']
+
+
+        users = get_user_model().objects.filter(enteredRoom=roompk).values("id","email","password","image","isReady","isMakingRoom","nickname","enteredRoom")
+        # userlist = []
+        # for us in users:
+        #     userlist.append(us.values())
+
+        response = Response()
+        response.headers = {
+            'access_token':new_access_token,
+        }
+        response.data = {
+            'message' : 'success',
+            'roomusers': users
+        }
+        response.status_code = status.HTTP_200_OK
+        return response
+
+
+
+class EnterRoomView(APIView):
+    def post(self, request):
+        access_token = request.headers.get("access-token")
+        refresh_token = request.headers.get("refresh-token")
+
+        pk = request.data['pk']
+        user = get_object_or_404(get_user_model(), pk=pk)
+        new_access_token, checkError = CheckToken(access_token,refresh_token,int(pk))
+        if checkError:
+            user.refreshToken = ''
+            user.save()
+            response = Response()
+            response.data = { 
+                'message' : 'failed',
+                'detail': 'Token Error',
+            }
+            response.status_code = status.HTTP_401_UNAUTHORIZED
+            return response
+
+        roompk = request.data['roompk']
+        user.enteredRoom = roompk
+        user.save()
+        users = get_user_model().objects.filter(enteredRoom=roompk).values("id","email","password","image","isReady","isMakingRoom","nickname","enteredRoom")
+        response = Response()
+        response.headers = {
+            'access_token':new_access_token,
+        }
+        response.data = {
+            'message' : 'success',
+            'roomusers': users
+        }
+        response.status_code = status.HTTP_200_OK
+        return response
+
